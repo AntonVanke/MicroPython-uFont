1b72af639487584ffb2eec32ff9bf0498ba9900c
antonvanke <antonvanke@hotmail.com>
2022-07-17 16:23:15 +0800

    INIT

---
+++ bitmapfonts.py
@@ -0,0 +1,77 @@
+# 生成点阵字体文件
+import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+
+# 需要转换的字符集并排序
+WORDS = list(set(list(open("3500.txt", encoding="utf-8").read())))
+WORDS.sort()
+# print(WORDS)
+# 转换为点阵文件的源字体文件
+FONT_SIZE = 16
+FONT = ImageFont.truetype(font='simsun.ttc', size=FONT_SIZE)
+
+# 生成的 bmf
+bitmap_fonts = open("test.bmf", "wb")
+
+
+def get_im(word, width, height):
+    im = Image.new('1', (width, height), (1,))
+    draw = ImageDraw.Draw(im)
+    draw.text((0, 0), word, font=FONT)
+    return im
+
+
+def to_bitmap(word):
+    code = 0x00
+    data_code = word.encode("utf-8")
+    width = 16
+    # fixme: 如果英文为 8 宽度无法存储
+    # if ord(word) < 128:
+    #     width = 8
+    # else:
+    #     width = 16
+    try:
+        for byte in range(len(data_code)):
+            code |= data_code[byte] << (len(data_code) - byte - 1) * 8
+    except IndexError:
+        print(word, word.encode("utf-8"))
+
+    bp = (~np.asarray(get_im(word, width=width, height=FONT_SIZE))).astype(np.int32)
+
+    bmf = []
+
+    for b in bp:
+        v = 0
+        for _ in b[0:8]:
+            v = (v << 1) + _
+        bmf.append(v)
+
+    for e in bp:
+        v = 0
+        for _ in e[8:]:
+            v = (v << 1) + _
+        bmf.append(v)
+
+    bitmap_fonts.write(bytearray(bmf))
+
+
+# 字节记录占位
+bitmap_fonts.write(bytearray([0x01, 0, 0, 0, 0x10, 0, 0, 0, 0]))  # 第一位存版本，中间三位存开始位置，第五位存字号
+
+for _ in WORDS:
+    bitmap_fonts.write(bytearray(_.encode("utf-8")))
+
+print("索引写入完毕", bitmap_fonts.tell(), hex(bitmap_fonts.tell()))
+start_bitmap = bitmap_fonts.tell()
+
+# 点阵开始字节写入
+bitmap_fonts.seek(0x01, 0)
+bitmap_fonts.write(bytearray([start_bitmap >> 16, start_bitmap - (start_bitmap >> 8 << 8)]))
+bitmap_fonts.seek(start_bitmap, 0)
+
+# 开始写入点阵
+for _ in WORDS:
+    to_bitmap(_)
+
+print("点阵写入完毕", bitmap_fonts.tell(), hex(bitmap_fonts.tell()))
+bitmap_fonts.close()
---
+++ bmftest.py
@@ -0,0 +1,22 @@
+with open("test.bmf", "rb") as bmf:
+    bmf.seek(0, 0)
+    start = bmf.read(2)
+    start = (start[0] << 8) + start[1]
+    bmf.seek(2, 0)
+    words = bmf.read(start - 2).decode("utf-8")
+
+bmf_file = open("test.bmf", "rb")
+
+
+def get_bitmap(word):
+    index = words.find(word)
+    if index == -1:
+        return [0x00, 0x7F, 0x7F, 0x67, 0x6B, 0x75, 0x7A, 0x7D, 0x7D, 0x7A, 0x75, 0x6B, 0x67, 0x7F, 0x7F, 0x00, 0x00,
+                0xFE, 0xFE, 0xE6, 0xD6, 0xAE, 0x5E, 0xBE, 0xBE, 0x5E, 0xAE, 0xD6, 0xE6, 0xFE, 0xFE, 0x00]
+    bmf_file.seek(start + 32 * index, 0)
+    return list(bmf_file.read(32))
+
+
+print(words)
+a = get_bitmap("!")
+print(a)
